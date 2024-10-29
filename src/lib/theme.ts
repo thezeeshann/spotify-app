1e7ae1d98416e3742412dc2ccaa8174a5d3d9acd
thezeeshann <info.zeeshann@gmail.com>
2024-10-29 23:40:31 +0530

    refactor: Added colors

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,6 +1,10 @@
 export const theme = {
   colorBlack: "#000000",
-  colorGreen: "#3ADF74",
   colorWhite: "#FFFFFF",
   colorGray: "#242424",
+  colorGreen: "#CFF56A",
+  colorPink: "#FFD2D7",
+  colorBlue: "#A5BBD1",
+  colorOrange: "#FFC862",
+  colorViolet: "#C4B1D4",
 };
