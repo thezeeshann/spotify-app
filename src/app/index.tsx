41c16488e63ec184868b5b988f51c589c1dd6fd9
thezeeshann <info.zeeshann@gmail.com>
2024-11-03 21:23:48 +0530

    refactor: Update components

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,9 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ScrollView, Text, View, Image } from "react-native";
+import PopularAlbums from "../components/home/popular-albums";
+import TopMixes from "../components/home/top-mixes";
+import RecommendedForYou from "../components/home/recommended-for-you";
+import Recents from "../components/home/recent";
 
 export default function App() {
   return (
@@ -9,7 +13,7 @@ export default function App() {
         showsHorizontalScrollIndicator={false}
       >
         <View className="flex flex-row flex-wrap items-center mt-6 gap-x-2 gap-y-2">
-          <View className="bg-[#CFF56A] rounded-full px-6 py-2">
+          <View className="bg-[#3BE377] rounded-full px-6 py-2">
             <Text className="text-[#333333] text-lg">All</Text>
           </View>
           <View className="bg-[#333333] rounded-full px-6 py-2">
@@ -59,93 +63,13 @@ export default function App() {
           </View>
         </View>
 
-        <View className="mt-8">
-          <Text className="text-[#FFFFFF]  font-bold text-2xl">
-            Your Top mixes
-          </Text>
-          <ScrollView
-            horizontal={true}
-            showsVerticalScrollIndicator={false}
-            showsHorizontalScrollIndicator={false}
-          >
-            <View className="flex flex-row mt-4 gap-x-4">
-              <Image
-                className="w-[150px] h-[150px]"
-                source={{
-                  uri: "https://seed-mix-image.spotifycdn.com/v6/img/artist/6DARBhWbfcS9E4yJzcliqQ/en/default",
-                }}
-              />
-              <Image
-                className="w-[150px] h-[150px]"
-                source={{
-                  uri: "https://seed-mix-image.spotifycdn.com/v6/img/artist/5r3wPya2PpeTTsXsGhQU8O/en/default",
-                }}
-              />
-              <Image
-                className="w-[150px] h-[150px]"
-                source={{
-                  uri: "https://seed-mix-image.spotifycdn.com/v6/img/artist/6DARBhWbfcS9E4yJzcliqQ/en/default",
-                }}
-              />
-              <Image
-                className="w-[150px] h-[150px]"
-                source={{
-                  uri: "https://seed-mix-image.spotifycdn.com/v6/img/artist/6DARBhWbfcS9E4yJzcliqQ/en/default",
-                }}
-              />
-              <Image
-                className="w-[150px] h-[150px]"
-                source={{
-                  uri: "https://seed-mix-image.spotifycdn.com/v6/img/artist/6DARBhWbfcS9E4yJzcliqQ/en/default",
-                }}
-              />
-            </View>
-          </ScrollView>
-        </View>
+        <TopMixes />
 
-        <View>
-          <Text className="text-[#FFFFFF]  font-bold text-2xl">
-            Popular albums
-          </Text>
-          <View>
-            <View>
-              <Image
-                className="w-[150px] h-[150px]"
-                source={{
-                  uri: "https://i.scdn.co/image/ab67616d00001e02aad3f4b601ae8763b3fc4e88",
-                }}
-              />
-              <Text></Text>
-            </View>
-            <View>
-              <Image
-                className="w-[150px] h-[150px]"
-                source={{
-                  uri: "https://i.scdn.co/image/ab67616d00001e021a8c4618eda885a406958dd0",
-                }}
-              />
-              <Text></Text>
-            </View>
-            <View>
-              <Image
-                className="w-[150px] h-[150px]"
-                source={{
-                  uri: "https://i.scdn.co/image/ab67616d00001e02bb25239324c4e16ae01fda36",
-                }}
-              />
-              <Text></Text>
-            </View>
-            <View>
-              <Image
-                className="w-[150px] h-[150px]"
-                source={{
-                  uri: "https://i.scdn.co/image/ab67616d00001e025f3ede47954a93aa03efe5f9",
-                }}
-              />
-              <Text></Text>
-            </View>
-          </View>
-        </View>
+        <PopularAlbums />
+
+        <Recents />
+
+        <RecommendedForYou />
       </ScrollView>
     </SafeAreaView>
   );
